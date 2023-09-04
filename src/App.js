293a4eff3b3c545aeba484fcffaed36c2c3af5a8
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-09-04 23:47:04 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const sv = {
+    backgroundColor: "red",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <svg width="150px" height="100px">
+        <circle cx="150" cy="50" r="50" stroke="blue" fill="none"></circle>
+      </svg>
     </div>
   );
 }
